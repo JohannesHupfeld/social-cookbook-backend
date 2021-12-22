4bd36b964c6e4c37518bc5fff8d628dceda9afbb
JohannesHupfeld <j.hupfeld@gmail.com>
2021-12-22 18:15:12 -0500

    removed aliasing in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :recipes
   has_many :comments
-  has_many :commented_recipes, through: :comments, source: :recipe
+  # has_many :commented_recipes, through: :comments, source: :recipe
 
   has_secure_password
 end
