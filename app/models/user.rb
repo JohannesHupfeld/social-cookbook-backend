6a98edfeafe65d35097f2802c02ef269a627cbc4
JohannesHupfeld <j.hupfeld@gmail.com>
2021-12-22 00:40:50 -0500

    has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   has_many :categories, through: :recipes
   has_many :comments
   has_many :commented_recipes, through: :comments, source: :recipe
+
+  has_secure_password
 end
