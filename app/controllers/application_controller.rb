d222ec7585f4960fe7c5ffca72472339c9711be5
JohannesHupfeld <j.hupfeld@gmail.com>
2021-12-22 18:51:39 -0500

    removed helper do

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+  def current_user
+    # MOCK of being logged in
+    User.first
+    # #If current user is already defined then itll equal @ current_user otherwise itll find by id in the users table
+    # @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id] #The if prevents calls to the database if no need
+    # #else already object  <--   then look for object   <--   if this
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  # def authorized_to_edit?
+  # end
 end
