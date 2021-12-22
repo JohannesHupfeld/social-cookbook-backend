cf41c0b9bf8dd91561e27d3deb8a146e642eb23b
JohannesHupfeld <j.hupfeld@gmail.com>
2021-12-22 11:21:49 -0500

    created recipe model, controller, routes with scaffold

---
+++ db/migrate/20211222161936_create_recipes.rb
@@ -0,0 +1,16 @@
+class CreateRecipes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :recipes do |t|
+      t.string :name
+      t.string :ingredients
+      t.string :instructions
+      t.string :image_url
+      t.integer :category_id
+      t.integer :user_id
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
