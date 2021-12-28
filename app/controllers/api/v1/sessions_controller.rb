f3b98d97f8bbf05568564b7e8aa21a6a01bb5f65
JohannesHupfeld <j.hupfeld@gmail.com>
2021-12-28 18:30:41 -0500

    get_current_user action method

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,12 +4,22 @@ class Api::V1::SessionsController < ApplicationController
     @user = User.find_by(username: params[:session][:username])
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-      render json: UserSerializer.new(@user), status: :ok
+      render json: @user
     else 
       render json: {
         error: "Invalid Information"
       }
     end 
+  end
+
+  def get_current_user
+    if logged_in?
+      render json: current_user
+    else
+      render json: {
+        error: "Not logged in"
+      }
+    end
   end
 
 
