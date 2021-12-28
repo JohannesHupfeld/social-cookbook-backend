6543e19214db9237bfd782853079c1e7e1b57a6f
JohannesHupfeld <j.hupfeld@gmail.com>
2021-12-28 14:35:48 -0500

    implemented sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
   def current_user
     # MOCK of being logged in
-    User.first
+    User.find_by(id: session[:user_id])
     # #If current user is already defined then itll equal @ current_user otherwise itll find by id in the users table
     # @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id] #The if prevents calls to the database if no need
     # #else already object  <--   then look for object   <--   if this
