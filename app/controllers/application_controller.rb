f1bc814a962f6aa6161ab30113b2cfb0bc203bd3
JohannesHupfeld <j.hupfeld@gmail.com>
2021-12-29 14:02:11 -0500

    changed code to prevent database calls if no need

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::API
   include ::ActionController::Cookies
 
   def current_user
-    User.find_by(id: session[:user_id])
+    # User.find_by(id: session[:user_id])
     # #If current user is already defined then itll equal @ current_user otherwise itll find by id in the users table
-    # @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id] #The if prevents calls to the database if no need
+    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id] #The if prevents calls to the database if no need
     # #else already object  <--   then look for object   <--   if this
   end
 
