8cc8d3123044c739eef874f2671c67cb1c248cbd
JohannesHupfeld <j.hupfeld@gmail.com>
2021-12-30 18:54:20 -0500

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   # has_many :commented_recipes, through: :comments, source: :recipe
 
   has_secure_password
+  validates :name, :username, :password, presence: true 
+  validates :username, uniqueness: true  
 end
