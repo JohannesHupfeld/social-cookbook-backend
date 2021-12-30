bff3c83964d54d485ba443a916cb7b2fbbb0e6d5
JohannesHupfeld <j.hupfeld@gmail.com>
2021-12-30 18:53:51 -0500

    added json errors, removed location and commented back in session user id

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -18,15 +18,14 @@
   # POST /users
   def create
     @user = User.new(user_params)
-
     if @user.save
-      # session[:user_id] = @user.id
-      render json: @user, status: :created, location: @user
+      session[:user_id] = @user.id
+      render json: UserSerializer.new(@user), status: :created
     else
       resp = {
         error: @user.errors.full_messages.to_sentence
       }
-      render json: @user.errors, status: :unprocessable_entity
+      render json: resp, status: :unprocessable_entity
     end
   end
 
