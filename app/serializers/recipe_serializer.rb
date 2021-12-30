8a8bb8c88cd0cfa3790a762f6c06c798da7632aa
JohannesHupfeld <j.hupfeld@gmail.com>
2021-12-30 01:09:09 -0500

    added comments to attributes and commented out has many comments and has many users through comments

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,8 +1,8 @@
 class RecipeSerializer
   include JSONAPI::Serializer
-  attributes :name, :ingredients, :instructions, :image_url, :user_id, :created_at, :updated_at
+  attributes :name, :ingredients, :instructions, :image_url, :user_id, :created_at, :updated_at, :comments
 
   belongs_to :user, serializer: UserSerializer
-  has_many :comments
-  has_many :users, through: :comments 
+   # has_many :comments
+   # has_many :users, through: :comments 
 end
